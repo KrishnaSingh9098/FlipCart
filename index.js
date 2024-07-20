3c7cd314f5bb7fb2d2e755cee73e72b6304edd1e
KrishnaSingh9098 <arushsingh8103@gmail.com>
2024-07-20 20:31:54 +0530

    20/07/2024

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+let mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/Amazon').then(()=>{
+    console.log('db...');
+}).catch((err)=>{
+    console.log(err)
+})
+const app = express()
+app.use(express.urlencoded({ extended: true }));
+
+let userRoutes = require ('./routes/user')
+let loginRoutes = require('./routes/login')
+
+
+app.get('/',(req,res)=>{
+    res.send("hyy")
+})
+
+app.use('/api',userRoutes)
+app.use('/api',loginRoutes)
+
+app.listen(3000,()=>{
+    console.log("SERVER IS RUNNING IN PORT NO :3000 ")
+})
